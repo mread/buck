34a0fa5bb1ebb1a1a072ba51f1c298b97e58edca
smithc <smithc@lmax.com>
2014-06-24 09:42:01 +0000

    Pass system property to test runner containing the base dir that the test belongs to.

--- test/com/facebook/buck/java/JUnitStepTest.java
+++ test/com/facebook/buck/java/JUnitStepTest.java
@@ -57,6 +57,9 @@ public class JUnitStepTest {
     BuildId pretendBuildId = new BuildId("pretend-build-id");
     String buildIdArg = String.format("-D%s=%s", JUnitStep.BUILD_ID_PROPERTY, pretendBuildId);
 
+    String modulePath = "module/submodule";
+    String modulePathArg = String.format("-D%s=%s", JUnitStep.MODULE_PATH_PROPERTY, modulePath);
+
     Path directoryForTestResults = Paths.get("buck-out/gen/theresults/");
     Path directoryForTemp = Paths.get("buck-out/gen/thetmp/");
     boolean isCodeCoverageEnabled = false;
@@ -68,6 +71,7 @@ public class JUnitStepTest {
         testClassNames,
         vmArgs,
         directoryForTestResults,
+        modulePath,
         directoryForTemp,
         isCodeCoverageEnabled,
         isDebugEnabled,
@@ -87,6 +91,7 @@ public class JUnitStepTest {
             "java",
             "-Djava.io.tmpdir=" + directoryForTemp,
             buildIdArg,
+            modulePathArg,
             vmArg1,
             vmArg2,
             "-verbose",
@@ -121,6 +126,9 @@ public class JUnitStepTest {
     BuildId pretendBuildId = new BuildId("pretend-build-id");
     String buildIdArg = String.format("-D%s=%s", JUnitStep.BUILD_ID_PROPERTY, pretendBuildId);
 
+    String modulePath = "module/submodule";
+    String modulePathArg = String.format("-D%s=%s", JUnitStep.MODULE_PATH_PROPERTY, modulePath);
+
     Path directoryForTestResults = Paths.get("buck-out/gen/theresults/");
     Path directoryForTemp = Paths.get("buck-out/gen/thetmp/");
     boolean isCodeCoverageEnabled = false;
@@ -132,6 +140,7 @@ public class JUnitStepTest {
         testClassNames,
         vmArgs,
         directoryForTestResults,
+        modulePath,
         directoryForTemp,
         isCodeCoverageEnabled,
         isDebugEnabled,
@@ -154,6 +163,7 @@ public class JUnitStepTest {
             "java",
             "-Djava.io.tmpdir=" + directoryForTemp,
             buildIdArg,
+            modulePathArg,
             "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005",
             vmArg1,
             vmArg2,
